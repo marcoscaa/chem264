fe2f50210c54c8a905dd62f485b9c531260f1993
Marcos Calegari <marcos.caa@gmail.com>
2025-04-09 13:25:38 -0700

    create input scripts

--- utils/create_pw_input_from_cif.py
+++ utils/create_pw_input_from_cif.py
@@ -1,13 +1,15 @@
-from ase import Atoms
-from ase.io import write
-from pymatgen.io.ase import AseAtomsAdaptor
-from pymatgen.ext.matproj import MPRester
 from pymatgen.core import Structure
+from pymatgen.io.cif import CifParser
+from ase.io import write
 from sys import argv
 
-def generate_pwscf_input_from_mp(structure, output_filename="pwscf.in"):
+def generate_pwscf_input_from_cif(structure, output_filename="pwscf.in"):
 
-    atoms = AseAtomsAdaptor.get_atoms(structure)
+    reader = CifParser(structure)
+    structure: Structure = reader.get_structures(primitive=True)[0]  # Get the first structure
+
+    # Convert Pymatgen Structure to ASE Atoms object
+    atoms: Atoms = structure.to_ase_atoms()
 
     input_params = {
         'control': {
@@ -43,29 +45,11 @@ def generate_pwscf_input_from_mp(structure, output_filename="pwscf.in"):
             kpts=tuple(kpoints_grid_config),
             input_data=input_params)
 
-def get_structure_from_mp(api_key, mp_id):
-    try:
-        with MPRester(api_key) as mpr:
-            structure = mpr.get_structure_by_material_id(mp_id)
-            if structure:
-                return structure
-            else:
-                print(f"No structure found for MP-ID: {mp_id}")
-                return None
-    except Exception as e:
-        print(f"Incorrect MP-ID '{mp_id}'")
-        return None
-
 
 if __name__ == "__main__":
     # Replace with the desired Materials Project ID and your API key
-    material_id_to_extract = argv[1] 
-    your_api_key = "qQccZYnGqnS0ad2qoZ92chnoQdBvKgSb"  # Replace with your actual API key
+    cif_filename = argv[1] 
     output_file = "pw.in"  # You can change the output filename
 
-    #Authenticate your Materials Project ID
-    structure = get_structure_from_mp(your_api_key,material_id_to_extract)
-    print(structure)
-
-    generate_pwscf_input_from_mp(structure, output_file)
+    generate_pwscf_input_from_cif(cif_filename, output_file)
 
