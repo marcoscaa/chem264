c14648bed24554804cac76f59067ced6b847da2a
Marcos Calegari <marcos.caa@gmail.com>
2025-04-27 18:10:11 -0700

    diamond

--- week5/01_change_alat/plot.sh
+++ week5/01_change_alat/plot.sh
@@ -1,5 +1,6 @@
-for alat in "5.2" "5.3" "5.4" "5.5" "5.6" "5.7" "5.72" "5.74" "5.76" "5.78" "5.8" "5.82" "5.84" "5.9" "6.0" "6.1" "6.2" "6.3" "6.4" "6.5" "6.6" 
+for file in scf_*.out 
 do 
+  alat=$(echo "$file" | sed 's/scf_\([0-9.]*\)\.out/\1/')
   a=`grep ! scf_${alat}.out | awk '{print $5}'` 
   echo $alat $a 
 done > data
@@ -13,7 +14,7 @@ plt.plot(f[0],f[1])
 plt.xlabel("alat (\$\\AA\$)")
 plt.ylabel("Energy (Ry)")
 plt.tight_layout()
-plt.savefig('cell_relax.pdf')
+plt.savefig('energy_alat.pdf')
 EOF
 
 python plot.py
