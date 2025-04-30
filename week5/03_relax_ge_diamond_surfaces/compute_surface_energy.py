d286ee2d9139704750c7c952b0c6b3c6be01b2ba
marcos.caa1 <marcos.caa@gmail.com>
2025-04-30 11:53:55 -0700

    Compute surface energy

--- week5/03_relax_ge_diamond_surfaces/compute_surface_energy.py
+++ week5/03_relax_ge_diamond_surfaces/compute_surface_energy.py
@@ -2,6 +2,10 @@ import numpy as np
 from ase.io import read
 from sys import argv
 
+#Unit conversion
+eVtoJ=1.60218e-19
+A2toM2=1.E-20
+
 #Read QE output file
 bulk = read(argv[1], index=-1)
 surface = read(argv[2], index=-1)
@@ -14,9 +18,14 @@ Ebulk /= len(bulk.get_atomic_numbers())
 Eslab = surface.get_potential_energy()
 N = len(surface.get_atomic_numbers())
 
+#Surface area
+cell=surface.get_cell()
+area=np.linalg.norm(np.cross(cell[0],cell[1]))
+
 #Surface energy
 Esurf = Eslab - N*Ebulk
-Esurf/= 2
+Esurf/= 2*area
+Esurf *= eVtoJ/A2toM2
 
 print(Esurf)
 
