82edc49a3eb48b705d01f4c28bdb681e8d52fc7f
marcos.caa1 <marcos.caa@gmail.com>
2025-06-03 14:24:55 -0700

    convert

---
+++ utils/convert_pwout_to_extxyz.py
@@ -0,0 +1,11 @@
+from ase.io import read, write
+from glob import glob
+from sys import argv
+
+# outcar contanining one (single-point) or multiple (trajectory) DFT frames
+in_filename = argv[1]
+out_filename = 'input.extxyz'
+
+# read all frames into a list of ase.Atoms objects
+all_atoms = read(in_filename, format='espresso-in', index=":")
+write(out_filename, all_atoms, format='extxyz')
