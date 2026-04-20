3138ca202bb1e05941fe67a8f4a4728c1325062d
Marcos Calegari <mandrade@princeton.edu>
2026-04-20 12:03:45 -0700

    Changing make_ge_surface - ASE atoms object is now in the conventional cell

--- week4/make_ge_surface.py
+++ week4/make_ge_surface.py
@@ -22,7 +22,7 @@ def generate_germanium_slab(
     a = 5.657  # Angstrom
 
     # Create the bulk FCC diamond structure
-    bulk_germanium = bulk("Ge", crystalstructure="diamond", a=a)
+    bulk_germanium = bulk("Ge", crystalstructure="diamond", a=a, cubic=True)
 
     # Create the (001) surface slab
     # The (0, 0, 1) miller index corresponds to the z-direction
@@ -33,12 +33,30 @@ def generate_germanium_slab(
         vacuum=vacuum / 2.0, # Vacuum on one side. ASE adds on both sides.
     )
 
+    slab = delete_undercoordinated_atoms(slab,"Ge","Ge",2.6,2)
+
     # Center the atoms in the cell.
     slab.center()
 
     # Save the structure in XYZ format
     slab.write(filename, format="extxyz")
 
+def delete_undercoordinated_atoms(atoms,atype,atype2,rcut,mincoord):
+    ind_atoms = [ atom.index for atom in atoms if atom.symbol == atype2 ]
+    coord_number = np.full(len(atoms.get_chemical_symbols()),10)
+    for ind, atom in enumerate(atoms):
+        if atom.symbol == atype:
+            distances = atoms.get_distances(atom.index,ind_atoms,mic=True)
+            filt=np.logical_and(distances<rcut,distances>0.1)
+            coord_number[ind] = len(distances[filt])
+            if len(distances[filt])==2:
+                print(distances)
+
+
+    print(coord_number)
+    del atoms[coord_number<mincoord]
+    return atoms
+
 if __name__ == "__main__":
     # Generate a 3-layer slab with 10 Å vacuum
-    generate_germanium_slab(layers=5, vacuum=10.0, index=(1,1,1))
+    generate_germanium_slab(layers=5, vacuum=10.0, index=(1,0,0))
